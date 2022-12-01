ad5946ebe574a66699542169e29cee8163970df5
Julien Adam <julien.adam@gmail.com>
2022-12-01 19:13:51 +0100

    Multithreaded version
    not 100% correct as the first thread to find a match wins so it could be
    a valid but incorrect response. much faster though

--- 2015/day04/day04.go
+++ 2015/day04/day04.go
@@ -3,6 +3,7 @@ package day04
 import (
 	"crypto/md5"
 	"fmt"
+	"sync"
 )
 
 func hashHas5Zeroes(input string) bool {
@@ -27,14 +28,34 @@ func hashHas6Zeroes(input string) bool {
 	return hash[0] == 0 && hash[1] == 0 && hash[2] == 0
 }
 
-func mineAdventCoinAdvanced(input string) int {
-	i := 0
+func threadMiner(input string, start int, step int, wg *sync.WaitGroup, result *int, stop *bool) {
+	i := start
 	for {
-		if hashHas6Zeroes(fmt.Sprintf("%s%d", input, i)) {
-			return i
+		if *stop {
+			break
 		}
-		i++
+		if hashHas6Zeroes(fmt.Sprintf("%s%d", input, i)) {
+			*result = i
+			wg.Done()
+			break
+		}
+		i += step
 	}
+}
+
+func mineAdventCoinAdvanced(input string) int {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	found := false
+	result := 0
+
+	for i := 0; i < 8; i++ {
+		go threadMiner(input, i, 8, &wg, &result, &found)
+	}
+
+	wg.Wait()
+	found = true
+	return result
 }
 
 func LoadAndSolvePart1() int {
