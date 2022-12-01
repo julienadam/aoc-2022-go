1f777913a8986e1c923ebe5ae42742b39290c85e
Julien Adam <julien.adam@gmail.com>
2022-12-01 10:51:44 +0100

    Started day 03 2015, stopped to make way for 2022

---
+++ 2015/day03/day03_test.go
@@ -0,0 +1,16 @@
+package day03
+
+// func deliver(input string) int {
+// 	var visited map[Vertex]bool
+// 	return 2
+// }
+
+// func Test_east_only_visits_2(t *testing.T) {
+// 	visited := deliver(">")
+// 	assert.Equal(t, 2, visited)
+// }
+
+// func Test_square_visits_4(t *testing.T) {
+// 	visited := deliver("^>v<")
+// 	assert.Equal(t, 4, visited)
+// }
