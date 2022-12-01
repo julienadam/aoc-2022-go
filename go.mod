e72d8a8c8e3b4b031fe1a1591edc810de66989b5
Julien Adam <julien.adam@gmail.com>
2022-12-01 16:35:18 +0100

    Added timing

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/assertgo/assert v2.0.0+incompatible // indirect
+	github.com/bradhe/stopwatch v0.0.0-20190618212248-a58cccc508ea // indirect
 	github.com/google/go-cmp v0.5.5 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	gotest.tools v2.2.0+incompatible // indirect
--- main.go
+++ main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"fmt"
 
+	"github.com/bradhe/stopwatch"
 	"github.com/julienadam/adventofcode2022/2022/day01"
 )
 
 func main() {
-	fmt.Printf("%d\n", day01.LoadAndSolvePart2())
+	sw := stopwatch.Start()
+	result := day01.LoadAndSolvePart1()
+	sw.Stop()
+	fmt.Printf("%d\nDone in %dms", result, sw.Milliseconds())
 }
-
-
-/*
-
-*/
